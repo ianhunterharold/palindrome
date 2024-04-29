0e510e1075e6548b689a8f1fdcfc41a8903850d2
Ian Harold <ianharold@Number-1.local>
2024-04-29 14:47:06 -0500

    crap solution

--- palindrome.js
+++ palindrome.js
@@ -16,3 +16,23 @@ var isPalindrome = function (s) {
         return false
     }
 };
+
+// WORSE SOLUTION FOR MY ANSWER while loop performed worse with runtime and memory used 
+const justLetters = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+
+let start = 0;
+let end = justLetters.length - 1;
+
+while (start <= end) {
+    console.log(justLetters[start],'START')
+    console.log(justLetters[end],'END')
+    if (justLetters[start] !== justLetters[end]) {
+       
+        return false
+    }
+    start++;
+    end--;
+}
+return true
+
+// crap solution but another solution
